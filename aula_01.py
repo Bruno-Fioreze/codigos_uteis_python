c4001270a1c6d40804924d97bde7003041fd8f0d
= <=>
2021-01-29 18:54:28 -0200

    first commit

---
+++ aula_01.py
@@ -0,0 +1,14 @@
+carrinho = []
+
+carrinho.append(("produto 1",20.0))
+carrinho.append(("produto 2",40.0))
+carrinho.append(("produto 3",8050))
+
+total = sum((v[1] for v in carrinho))
+
+
+print(total)
+
+##
+
+
