eddce412a61747c2a2a0f6fe554a6ac068e5c257
= <=>
2021-01-31 18:36:32 -0300

    atualizando rep

--- aula_01.py
+++ aula_01.py
@@ -1,3 +1,4 @@
+"""ex 1
 carrinho = []
 
 carrinho.append(("produto 1",20.0))
@@ -8,7 +9,14 @@ total = sum((v[1] for v in carrinho))
 
 
 print(total)
+"""
 
-##
 
+""" ex 2"""
+"""cidade = ["teste","teste3","teste5",'12321']
+estado = ["1",'2','3']
+from itertools import zip_longest
+for indice in zip_longest(cidade,estado):
+    print(indice)"""
 
+"""ex 3"""
